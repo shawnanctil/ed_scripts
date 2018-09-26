b2cbb990d5d42fd4442b4030c5ac961969ddd4f0
shawnanctil <shawnanctil@gmail.com>
2018-09-26 08:24:13 -0400

    Add files via upload
    Added a handful of new functions around fuzzy matching. Needs improvement.

--- CIP/cip_functions.py
+++ CIP/cip_functions.py
@@ -6,6 +6,7 @@ import nltk.data
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import TreebankWordTokenizer
+from fuzzywuzzy import fuzz,process
 
 '''
 Static variables
@@ -30,18 +31,50 @@ def get_synset(word):
 def get_def(syn):
     return wn.synset(syn).definition()
 
+# Get lemmas
+
+def get_lemma(syn):
+    for word in wn.synset(syn).lemmas():
+        return word.name()
+
+# Assess fuzzy matching ratio
+
+def get_fuzz(string,sectormatrix):
+    r = process.extract(string,sectormatrix,limit=10,scorer=fuzz.token_sort_ratio)
+    results = [s for s in r if s[1] >70]
+    return len(results)
+
+# Matches against code and returns top 2 values
+
+def cip_fuzz_value(string,sectormatrix):
+    r = process.extract(string,sectormatrix,limit=2,scorer=fuzz.token_sort_ratio)
+    results = [s for s in r]
+    return results
+
 # Creates a dataframe called sector_matrix based on another dataframe's column. Should be followed with an export.
 
-def sector_tagger(frame):
+def sector_tag_en(frame):
     tok_list = [tok.tokenize(w) for w in frame]
     split_words = [w.lower() for sub in tok_list for w in sub]
     clean_words = [w for w in split_words if w not in english_stops]
     synset = [get_synset(w) for w in clean_words]
+    lemmas = [get_lemma(w) if w != None else '' for w in synset]
     sector_matrix = DataFrame({'Categories': clean_words,
-                               'Synsets': synset})
+                               'Synsets': synset,
+                               'Lemmas': lemmas})
     sec_syn = list(sector_matrix['Synsets'])
     sector_matrix['Definition'] = [get_def(w) if w != None else '' for w in sec_syn]
     return sector_matrix
+
+
+def sector_tag_b(frame,sector):
+    tok_list = [tok.tokenize(w) for w in frame]
+    split_words = [w.lower() for sub in tok_list for w in sub]
+    clean_words_en = [w for w in split_words if w not in english_stops]
+    clean_words = [w for w in clean_words_en if w not in french_stops]
+    word_frame = DataFrame({'Words': clean_words,
+                               'Sector': sector})
+    return word_frame
 					
 # Create function to append an excel sheet with output of dataframes
 
